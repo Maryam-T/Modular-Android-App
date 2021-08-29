01732f942b3b541e64cc17744be732f8a70d37d6
Maryam Dashti <mry.dashti3@gmail.com>
2021-08-29 19:08:26 +0430

    fix HomeFragmentTest

--- app/src/androidTest/java/com/marand/myapplication/ui/main/home/view/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/marand/myapplication/ui/main/home/view/HomeFragmentTest.kt
@@ -1,10 +1,8 @@
 package com.marand.myapplication.ui.main.home.view
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
-import androidx.test.annotation.UiThreadTest
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -14,74 +12,64 @@ import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.google.common.truth.Truth.assertThat
 import com.marand.myapplication.R
-import com.marand.myapplication.ui.factory.user.UsersFactoryAndroidTest
 import com.marand.myapplication.ui.main.MainActivity
-import com.marand.myapplication.ui.main.home.adapter.UserAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
-@RunWith(AndroidJUnit4::class)
 @LargeTest
+@RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
 
     @get:Rule
-    var activityTestRule = ActivityTestRule(MainActivity::class.java, false, false)
+    var activityTestRule = ActivityTestRule(MainActivity::class.java)
 
-    private lateinit var userAdapter: UserAdapter
+    private val LIST_ITEM_POSITION_IN_TEST = 0;
 
-    @UiThreadTest
     @Test
     fun clickUserItem_navigateToPostFragment() {
         //Arrange
         val navController = launchHomeFragment()
 
-        setUpRecyclerView()
-
         //Act
         onView(withId(R.id.userListRv))
             .perform(
                 RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                    0,
+                    LIST_ITEM_POSITION_IN_TEST,
                     click()
                 )
             )
 
         //Assert
+        verify(navController).navigate(R.id.action_homeFragment_to_postFragment)
         assertThat(navController.currentDestination?.id).isEqualTo(R.id.postFragment)
-    }
+}
 
+    /**
+     * In addition to returning a new instance of our Fragment,
+     * get a callback whenever the fragment’s view is created
+     * or destroyed so that we can set the mock NavController
+     */
     private fun launchHomeFragment(): NavController {
         val navController = mock(NavController::class.java)
 
-        /*val homeFragmentScenario = launchFragmentInContainer<HomeFragment>()
-        homeFragmentScenario.onFragment { fragment ->
-            navController.setGraph(R.navigation.nav_graph)
-            Navigation.setViewNavController(fragment.requireView(), navController)
-        }*/
-
         HomeFragment().also { fragment ->
-            // In addition to returning a new instance of our Fragment,
-            // get a callback whenever the fragment’s view is created
-            // or destroyed so that we can set the mock NavController
-            fragment.viewLifecycleOwnerLiveData.observeForever { viewLifecycleOwner ->
-                if (viewLifecycleOwner != null) {
-                    // The fragment’s view has just been created
-                    navController.setGraph(R.navigation.nav_graph)
-                    Navigation.setViewNavController(fragment.requireView(), navController)
+            GlobalScope.launch(Dispatchers.Main) {
+                fragment.viewLifecycleOwnerLiveData.observeForever { viewLifecycleOwner ->
+                    if (viewLifecycleOwner != null) {
+                        //The fragment’s view has just been created
+                        navController.setGraph(R.navigation.nav_graph)
+                        Navigation.setViewNavController(fragment.requireView(), navController)
+                    }
                 }
             }
         }
 
         return navController
-    }
-
-    private fun setUpRecyclerView() {
-        val listOfUserItemView = UsersFactoryAndroidTest.generateListOfUserItemViews(5)
-
-        userAdapter = UserAdapter(listOfUserItemView)
-
-        activityTestRule.activity.findViewById<RecyclerView>(R.id.userListRv).adapter = userAdapter
     }
 }
